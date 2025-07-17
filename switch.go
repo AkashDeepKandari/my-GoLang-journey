0258d1e7067636345d1849397de5d4285c6fa161
AkashDeepKandari <kandariakashdeep39@gmail.com>
2025-07-17 08:10:59 -0700

    Basic switch Statement

---
+++ switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	i = 3
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+}
