1760654ec1168d38185c78ffdb2b75e05042f118
AkashDeepKandari <kandariakashdeep39@gmail.com>
2025-07-17 08:17:44 -0700

    mutiple_condition_switch

---
+++ mutiple_condition_switch.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("weekend")
+	default:
+		fmt.Println("Wortking days")
+	}
+}
