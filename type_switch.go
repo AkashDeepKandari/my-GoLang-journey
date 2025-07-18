1b8b0a835f94b4aed5383409289c0e2649a3066c
AkashDeepKandari <kandariakashdeep39@gmail.com>
2025-07-18 05:44:39 -0700

    type_switch

---
+++ type_switch.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	whoAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case int:
+			fmt.Println("this is int", t)
+		case bool:
+			fmt.Println("this is boolean", t)
+		case string:
+			fmt.Println("this is string", t)
+
+		}
+	}
+	//whoAmI(10)
+	whoAmI("Hello")
+}
