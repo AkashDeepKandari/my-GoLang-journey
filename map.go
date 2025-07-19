37caa572970c22b5bb3e11a3bbb0319b365423c5
AkashDeepKandari <kandariakashdeep39@gmail.com>
2025-07-19 08:18:33 -0700

    Day 3- Map Basics

---
+++ map.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[int]string)
+	m[1] = "AkashDeep Kandari"
+	m[2] = "Aniket Rawat"
+	m[3] = "Guppi"
+	m[4] = "Sam"
+	m[5] = "Om"
+	fmt.Println(m)
+	//for loop
+	for i := 1; i <= 5; i++ {
+		fmt.Println(m[i])
+	}
+	//for range
+	for i := range 3 {
+		fmt.Println(m[i])
+
+	}
+}
